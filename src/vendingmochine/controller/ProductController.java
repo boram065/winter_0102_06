3f8b5ed8f273edc0498c3c6fc2605c7a15d41de3
boram065 <boram4784@gmail.com>
2023-01-02 15:34:41 +0900

    add GUI

--- src/vendingmochine/controller/ProductController.java
+++ src/vendingmochine/controller/ProductController.java
@@ -17,12 +17,15 @@ public class ProductController extends JFrame{
 		ProductView view = new ProductView();
 		fullProduct();
 		view.setProductList(productList);
-		JPanel jpC = view.displayProducts();
+		JPanel jpC = view.displayProducts(this);
+		JPanel jpS = view.inputPurchase();
 		add(jpC, "Center");
+		add(jpS, "South");
 		
 		setTitle("음료 자판기");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBounds(100, 50, 600, 600);
+		setBounds(470, 20, 600, 750);
+		setResizable(false);
 		setVisible(true);
 	}
 	
@@ -30,7 +33,7 @@ public class ProductController extends JFrame{
 		//자판기 제품 채우기
 		ProductDAO dao = new ProductDAO();
 		String[] names = {"콜라", "사이다", "밀키스", "알로에", "처음처럼", "참이슬", "이슬톡톡", "테라", "카스"};
-		int[] prices = {1000, 1000, 1500, 2000, 1500, 1600, 3000, 2000, 2000};
+		int[] prices = {1500, 1500, 1900, 1800, 2000, 2500, 1500, 2300, 2200};
 		ProductVO pvo = null;
 		productList = dao.select();
 		for(int i = 0; i < names.length; i++) {
--- src/vendingmochine/view/ProductView.java
+++ src/vendingmochine/view/ProductView.java
@@ -1,36 +1,112 @@
 package vendingmochine.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import vendingmochine.vo.ProductVO;
 
 public class ProductView {
-
+	ProductVO pvo;
 	ArrayList<ProductVO> productList = new ArrayList<ProductVO>();
 	ArrayList<JLabel> jlList = new ArrayList<JLabel>();
+	JTextField jtf = new JTextField(10);
+	JLabel result = new JLabel("");
+	JFrame frame;
+	JLabel chagejl;
 	
-	public JPanel displayProducts() {
+	public JPanel displayProducts(JFrame frame) {
+		this.frame = frame;
 		JPanel jpC = new JPanel(new GridLayout(3,3));
+		JLabel pricejl = null;
 		for (ProductVO vo : productList) {
 			ImageIcon icon = new ImageIcon("images/" + vo.getImageName() + ".jpg");
 			JLabel jl = new JLabel(icon);
+			jl.setOpaque(true);
+			jl.setBackground(Color.WHITE);
+			jl.addMouseListener(jlL);
 			jlList.add(jl);
-			jpC.add(jl);
+			pricejl = new JLabel(vo.getPrice() + "원", JLabel.CENTER);
+			pricejl.setFont(new Font("한컴 고딕", Font.BOLD, 15));
+			pricejl.setOpaque(true);
+			pricejl.setBackground(Color.WHITE);
+			JPanel jp = new JPanel(new BorderLayout());
+			jp.add(jl, "Center");
+			jp.add(pricejl, "South");
+			jpC.add(jp);
 		}//for
 		return jpC;
 	}
 	
-	public void inputPurchase() {
-		
+	public JPanel inputPurchase() {
+		JPanel jpS = new JPanel();
+		JLabel money = new JLabel("금액 : ");
+		JButton jb = new JButton("투입");
+		jb.addActionListener(jbL);
+		jpS.add(money); jpS.add(jtf); jpS.add(jb); jpS.add(result);
+		return jpS;
 	}
 	
 	public void setProductList(ArrayList<ProductVO> productList) {
 		this.productList = productList;
 	}
+	
+	MouseAdapter jlL = new MouseAdapter() {
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			chagejl = (JLabel)e.getSource();
+			for(int i = 0; i < jlList.size(); i++) {
+				if(e.getSource() == jlList.get(i)) {
+					result.setIcon(null);
+					chagejl.setBackground(Color.RED);
+					pvo = productList.get(i);
+				}//if
+			}//for
+			JOptionPane.showMessageDialog(frame, "제품명 : " + pvo.getName() + "\n제품 가격 : "+ pvo.getPrice() + "원");
+		}
+		
+	};//jlL
+	
+	ActionListener jbL = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//투입버튼이 클릭됐을 때 JTextField 입력된 금액과 현재 금액을 비교하여 제품과 거스름 돈이 나오게한다
+			int money = Integer.parseInt(jtf.getText());
+			if(pvo.getPrice() < money) {
+				result.setIcon(null);
+				ImageIcon icon = new ImageIcon("images/" + pvo.getImageName() + ".jpg");
+				result.setIcon(icon);
+				JOptionPane.showMessageDialog(frame, "거스름돈 : " + (money - pvo.getPrice()) + "원");
+				jtf.setText("");
+			} else if(pvo.getPrice() > money) {
+				result.setIcon(null);
+				result.setText("돈 부족");
+				jtf.setText("");
+			} else if(pvo.getPrice() == money) {
+				result.setIcon(null);
+				ImageIcon icon = new ImageIcon("images/" + pvo.getImageName() + ".jpg");
+				result.setIcon(icon);
+				jtf.setText("");
+			}//if
+			chagejl.setBackground(Color.WHITE);
+		}
+		
+	};//jbL
 	
 }//class
